1fc78141e12e1792a154a618ee4f4b032b9524a1
ysw1227 <146154782+ysw1227@users.noreply.github.com>
2023-11-12 18:53:07 -0500

    Update scheme_eval_apply.py

--- scheme/scheme_eval_apply.py
+++ scheme/scheme_eval_apply.py
@@ -45,10 +45,17 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        list=[]
+        while args is not nil:
+            list. append(args.first)
+            args=args.rest
+        if procedure.need_env:
+            list.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            return procedure.py_func(*list)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
